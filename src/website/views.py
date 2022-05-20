6c5b345e9bb03498d764cd82a6f582422fa82ff1
marv nwadike <marvnwadike1@gmail.com>
2022-05-20 15:17:41 -0700

    base set up

---
+++ src/website/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint,render_template,flash,request,redirect,url_for
+from flask_login import login_required, current_user
+from . import db
+
+views = Blueprint("views",__name__)
+@views.route("/")
+@views.route("/home")
+def home():
+    return "<p>ChatAPP</p>"
